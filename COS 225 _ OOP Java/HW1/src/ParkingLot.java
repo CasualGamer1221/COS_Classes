ce91868940f339fa9e570a713fc26e1702704d1a
CasualGamer1221 <parentjeffery@gmail.com>
2022-02-07 12:37:10 -0500

    FINISHED

--- COS 225 _ OOP Java/HW1/src/ParkingLot.java
+++ COS 225 _ OOP Java/HW1/src/ParkingLot.java
@@ -34,6 +34,7 @@ public class ParkingLot {
 		return handicapped + " " + normal + "\n";
 	}
 	
+	//parks cars in appropriate spot
 	public int park(Car car){
 		for (int i = 0; i < 20; i++) {
 			if (spots[i].car1 == null && spots[i].handicap == car.handicapAccessible) {
@@ -48,6 +49,7 @@ public class ParkingLot {
 		return -1;
 	}
 	
+	//un-parks a specified car
 	public Car leave(int index) {
 		Car cartype = spots[index].car1;
 		spots[index].car1 = null;
--- COS 225 _ OOP Java/HW1/src/ParkingTester.java
+++ COS 225 _ OOP Java/HW1/src/ParkingTester.java
@@ -2,22 +2,22 @@
 public class ParkingTester {
 
 	public static void main(String[] args) {
+		//creates a new parkingLot and prints empty lot
 		ParkingLot lot1 = new ParkingLot();
-		
 		System.out.print(lot1.toString());
 		
+		//creates a new car and parks it
 		Car car1 = new Car("Lexus","RX","Blue",true);
 		int car1_index = lot1.park(car1);
-		
 		System.out.print(lot1.toString());
 		
+		//creates a second car and parks it
 		Car car2 = new Car("Ford","Explorer","",false);
 		lot1.park(car2);
-		
 		System.out.println(lot1.toString());
 		
+		//un-parks the first car
 		lot1.leave(car1_index);
-		
 		System.out.println(lot1.toString()); 
 	}
 
