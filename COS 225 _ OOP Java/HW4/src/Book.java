32f7d02a49f7116493634c3e9a49488eacfd34ca
CasualGamer1221 <parentjeffery@gmail.com>
2022-03-09 13:07:22 -0500

    start

---
+++ COS 225 _ OOP Java/HW4/src/Book.java
@@ -0,0 +1,13 @@
+
+public class Book {
+	String title , genre;
+	
+	public Book(String title,String genre) {
+		this.title = title;
+		this.genre = genre;
+	}
+	
+	public String toString() {
+		return title + "\n";
+	}
+}
---
+++ COS 225 _ OOP Java/HW4/src/LibraryTester.java
@@ -0,0 +1,9 @@
+
+public class LibraryTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
